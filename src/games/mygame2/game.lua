5fa992745928062b3773b4872e392e59e64b7ef4
Mitchell Skiba <git@mskiba.net>
2012-03-09 23:13:02 -0500

    Well, game modes work, sortof - it always passes easy

---
+++ src/games/mygame2/game.lua
@@ -0,0 +1,45 @@
+return {
+    name = "mygame",
+    diff = {},
+    PR = {"sse"},
+    keys = {"wasd"},
+    makeGameInstance = function(self, info)
+        self.getReady = function(self)
+            self.score = 0
+			self.pressed = 0
+        end
+        
+        self.update = function(self, dt)
+        end
+
+        self.draw = function(self)
+            love.graphics.setColor(255,255,255)
+            love.graphics.print("HOLD THE SPACEBAR!", 20, 20)    
+            love.graphics.setColor( 20+200*(self.pressed), 20,20)
+            love.graphics.rectangle('fill', 100,100, 150,150)
+        end
+
+        self.keypressed = function(self, key)
+            if key==" " then
+                self.score = 0
+				self.pressed = 1
+            end
+        end
+
+        self.keyreleased = function(self, key)
+            if key==" " then
+                self.score = 1
+				self.pressed = 0
+            end
+        end
+        
+        self.getScore = function(self, key)
+            return self.score
+        end
+
+        self.isDone = function(self,key)
+            return self.score > 0
+        end
+        
+    end
+}
