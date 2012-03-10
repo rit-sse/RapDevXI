1992463e1920554d560c7d8c9bfe3472be4feff7
Mitchell Skiba <git@mskiba.net>
2012-03-10 12:43:33 -0500

    Added "back to game mode menu" to pick gameMode

--- src/gameModes/pick.lua
+++ src/gameModes/pick.lua
@@ -5,12 +5,13 @@ local makeMode = function()
 		games = {},
 		sel = 1,
 		dif = 1,
+		continue = true,
 		givePick = false, --start opposite as we get the results of the gameMode first
 		setGameList = function(self,list)
 			self.games = list
 		end,
 		hasNextGame = function(self)
-			return true
+			return self.continue
 		end,
 		nextGame = function(self)
 			if self.givePick then
@@ -37,25 +38,39 @@ local makeMode = function()
 			self.selector = mode
 			self.draw = function(self)
 				love.graphics.setColor(255,255,255)
-				love.graphics.print(self.selector.sel..": "..(self.selector.games[self.selector.sel].name),10,200)
-				love.graphics.print(self.selector.games[self.selector.sel].difficulties[self.selector.dif],10,200+20)
+				
+				if self.selector.sel ~=0 then
+					love.graphics.print(self.selector.sel..": "..(self.selector.games[self.selector.sel].name),10,200)
+					love.graphics.print(self.selector.games[self.selector.sel].difficulties[self.selector.dif],10,200+20)
+				else
+					love.graphics.print("Back to game mode menu",10,200)
+				end
+				
+				
 			end
 			self.keypressed = function(self,key)
 				if key=='up' then
-					self.selector.sel = ((self.selector.sel-2)%(#self.selector.games))+1
+					self.selector.sel = ((self.selector.sel-1)%(#self.selector.games+1))
 					self.selector.dif = 1
 				end
 				if key=='down' then
-					self.selector.sel = ((self.selector.sel)%(#self.selector.games))+1
+					self.selector.sel = ((self.selector.sel+1)%(#self.selector.games+1))
 					self.selector.dif = 1
 				end
 				if key=='right' then
-					self.selector.dif = ((self.selector.dif)%(#self.selector.games[self.selector.sel].difficulties))+1
+					if self.selector.sel ~= 0  then
+						self.selector.dif = ((self.selector.dif)%(#self.selector.games[self.selector.sel].difficulties))+1
+					end
 				end
 				if key=='left' then
-					self.selector.dif = ((self.selector.dif-2)%(#self.selector.games[self.selector.sel].difficulties))+1
+					if self.selector.sel ~= 0 then
+						self.selector.dif = ((self.selector.dif-2)%(#self.selector.games[self.selector.sel].difficulties))+1
+					end
 				end
 				if key=='return' then
+					if self.selector.sel == 0 then
+						self.selector.continue = false
+					end
 					self.done = true
 				end
 			end
