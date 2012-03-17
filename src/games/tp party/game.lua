c56a2cb01238540a1c47b1207c536589d72115d0
Ricky Kendrick <Porian36@yahoo.com>
2012-03-16 21:59:32 -0400

    scaled the hands

--- src/games/tp party/game.lua
+++ src/games/tp party/game.lua
@@ -12,8 +12,8 @@ return {
             self.box1 = {x = self.cir1.x, y = self.cir1.y-self.cir1.r, w = 200, l = 2*self.cir1.r } -- box
             self.box2 = {x = self.cir1.x + self.cir1.r, y = self.cir1.y, w = 200, l = 800} -- toliet paper
             self.cir2 = {x = self.cir1.x + self.box2.w, y = self.cir1.y, r = self.cir1.r} -- the circle right
-            self.handL = {x = self.cir1.x + 25 + self.cir1.r, y = 200} -- left hand
-            self.handR = {x = self.cir2.x + -25 + self.cir1.r, y = 150} -- right hand
+            self.handL = {x = self.cir1.x + 36 + self.cir1.r, y = 200} -- left hand
+            self.handR = {x = self.cir2.x + -36 + self.cir1.r, y = 150} -- right hand
 			self.handLoc = {up = 150, down = 200} --up and down values
 			
             self.handL.img = love.graphics.newImage("hand.png")
@@ -26,7 +26,7 @@ return {
                 self.handR.y = self.handLoc.down
                 self.handL.y = self.handLoc.up
                 if self.cir1.r > 5 then -- 5 is the end goal
-                    self.cir1.r = self.cir1.r - 1
+                    self.cir1.r = self.cir1.r - .75
                 else
                     self.done = true
                 end
@@ -44,8 +44,8 @@ return {
             self.box2 = {x = self.cir1.x + self.cir1.r, y = self.cir1.y, w = 200, l = 800} -- toliet paper
             self.cir2 = {x = self.cir1.x + self.box2.w, y = self.cir1.y} -- the circle right
 
-			self.handL.x = self.cir1.x + 25 + self.cir1.r -- left hand
-            self.handR.x = self.cir2.x + -25 + self.cir1.r -- right hand			
+			self.handL.x = self.cir1.x + 36 + self.cir1.r -- left hand
+            self.handR.x = self.cir2.x + -36 + self.cir1.r -- right hand			
 		end
 		
 		self.draw = function(self)
@@ -58,8 +58,8 @@ return {
 			love.graphics.circle("fill", self.cir2.x, self.cir2.y, self.cir1.r, 100)
 			love.graphics.rectangle("fill", self.box1.x, self.box1.y, self.box1.w, self.box1.l)
 			love.graphics.rectangle("fill", self.box2.x, self.box2.y, self.box2.w, self.box2.l)
-			love.graphics.draw(self.handL.img, self.handL.x, self.handL.y, 0, -2, 2)
-			love.graphics.draw(self.handR.img, self.handR.x, self.handR.y, 0, 2, 2)
+			love.graphics.draw(self.handL.img, self.handL.x, self.handL.y, 0, -3, 3)
+			love.graphics.draw(self.handR.img, self.handR.x, self.handR.y, 0, 3, 3)
 		end
 		
 		self.isDone = function(self)
