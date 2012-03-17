401c5d4167586279fd3399e5023c625921ef1d57
Ricky Kendrick <Porian36@yahoo.com>
2012-03-16 23:39:52 -0400

    made the roll turn brown and the paper fall when done

--- src/games/tp party/game.lua
+++ src/games/tp party/game.lua
@@ -41,23 +41,33 @@ return {
 			self.elapsed_time = self.elapsed_time+dt	
 
             self.box1 = {x = self.cir1.x, y = self.cir1.y - self.cir1.r, w = 200, l = 2*self.cir1.r } -- box
-            self.box2 = {x = self.cir1.x + self.cir1.r, y = self.cir1.y, w = 200, l = 800} -- toliet paper
+			if self.done == false then
+				self.box2 = {x = self.cir1.x + self.cir1.r, y = self.cir1.y, w = 200, l = 800} -- toliet paper
+			end
             self.cir2 = {x = self.cir1.x + self.box2.w, y = self.cir1.y} -- the circle right
 
 			self.handL.x = self.cir1.x + 36 + self.cir1.r -- left hand
-            self.handR.x = self.cir2.x + -36 + self.cir1.r -- right hand			
+            self.handR.x = self.cir2.x + -36 + self.cir1.r -- right hand
+
+			if self.done == true then
+				self.box2.y = self.box2.y + .3
+			end
 		end
 		
 		self.draw = function(self)
             
 			--here we just put how much time is left in the upper left corner
 			-- look at https://love2d.org/wiki/love.graphics for fun drawing stuff
+			love.graphics.setColor(255,255,255)
 			love.graphics.print( (self.time_limit-self.elapsed_time).."s left", 0,0)
+			love.graphics.rectangle("fill", self.box2.x, self.box2.y, self.box2.w, self.box2.l)
 			
+			if self.done == true then
+				love.graphics.setColor(205,183,158)
+			end
 			love.graphics.circle("fill", self.cir1.x, self.cir1.y, self.cir1.r, 100)
 			love.graphics.circle("fill", self.cir2.x, self.cir2.y, self.cir1.r, 100)
 		    love.graphics.rectangle("fill", self.box1.x, self.box1.y, self.box1.w, self.box1.l)
-			love.graphics.rectangle("fill", self.box2.x, self.box2.y, self.box2.w, self.box2.l)
 	        love.graphics.setColor(130,130,130)
             love.graphics.circle("fill", self.cir1.x, self.cir1.y, 3, 100)
             love.graphics.setColor(255,255,255)
