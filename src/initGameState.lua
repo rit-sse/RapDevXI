77bf9d00efab89dcea3caccaea5fa6b0b3cb088c
Mitchell Skiba <git@mskiba.net>
2012-03-17 01:35:13 -0400

    Better mode screens, DDR has icons

--- src/initGameState.lua
+++ src/initGameState.lua
@@ -1,5 +1,7 @@
 local framework = require('framework')
 
+local keyrep = 0.15
+
 local loadGame = function(gameName)
 	local gameClass = nil
 	if pcall(function()
@@ -53,18 +55,59 @@ local initState =  function()
         return self.done
     end
 
-    base.draw = function(self, dt)
-        love.graphics.setColor(255,255,255)
-        output = self.currentPosition..": "
-        output = output..self.listOfGames[self.currentPosition][1].." | "
-        output = output..((self.listOfGames[self.currentPosition][2]) and "on" or "off")
-        love.graphics.print("Press 'a' to select all games", 10, love.graphics.getHeight() / 4)
-        love.graphics.print("Press 'o' to turn of all games", 10, love.graphics.getHeight() / 4 + 20)
-        love.graphics.print(output, 10, love.graphics.getHeight()/2)
-        love.graphics.print("PRESS ENTER TO CONTINUE", 10, (love.graphics.getHeight()/2)+20)
+    base.keytime = 0
+    base.keyed = false
+    base.key = ""
+    
+    base.update = function(self, dt)
+	self.keytime = self.keytime - dt
+	if self.keytime <0 then
+		self.keytime = keyrep
+		if self.keyed then
+			self:keypressed(self.key)
+		end
+		
+	end
+    end
+    
+    base.draw = function(self)
+	local pm = 9
+	local by = 15+15*(pm)
+	local ny = by+15*(pm+1)+10
+	
+	love.graphics.setColor(20,20,20)
+	love.graphics.rectangle("fill",5,5,love.graphics.getWidth()-10, love.graphics.getWidth()-10)
+	love.graphics.setColor(255,255,255)
+        love.graphics.rectangle('line',10,by-pm*15-5,love.graphics.getWidth()-20,15*(pm*2+1)+10)
+	love.graphics.rectangle('line',10,ny,love.graphics.getWidth()-20,love.graphics.getHeight()-ny-10)
+	
+	love.graphics.print('>', 5,by)
+	love.graphics.print('>', 10,by)
+	
+        for i=self.currentPosition-pm,self.currentPosition+pm do
+		local reali = i
+		if reali < 1 then reali = reali+#self.listOfGames end
+		if reali > #self.listOfGames then reali = reali-#self.listOfGames end
+	
+		local color = (self.listOfGames[reali][2]) and 255 or 60
+		love.graphics.setColor( color,color,color)
+		
+		local output = self.listOfGames[reali][1]
+		love.graphics.print(output, 20, by+(i-self.currentPosition)*15)
+	end
+	
+	love.graphics.setColor(255,255,255)
+        love.graphics.print("Press Left/Right to enable/disable selected game", 15, ny+5)
+        love.graphics.print("Press 'a' to select all games", 15, ny+20)
+	love.graphics.print("Press 'o' to turn of all games", 15, ny+35)
+        love.graphics.print("PRESS ENTER TO CONTINUE", 15, ny+50)
     end 
 
     base.keypressed = function(self, key)
+	self.keyed = true
+	self.key = key
+	self.keytime = keyrep
+    
         if key == 'up' then
             self.currentPosition = ((self.currentPosition -2) % #self.listOfGames)+1
         end
@@ -94,6 +137,10 @@ local initState =  function()
             self.done = true
         end
     end 
+    
+    base.keyreleased = function(self, key)
+	self.keyed = false
+    end
 
     framework.mode = framework.modes.chooser
         framework.limit = -1 -- about to be in the game menu, no limit on time
